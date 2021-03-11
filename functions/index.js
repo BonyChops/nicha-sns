7aba669dcd201ff1e703645f5fd01a74f34dddb3
BonyChops <office.kentarou@gmail.com>
2021-03-11 17:51:00 +0900

    Edit: functions test

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,10 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+admin.initializeApp();
+const express = require('express');
+const cookieParser = require('cookie-parser')();
+const cors = require('cors')({ origin: true });
+const app = express();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +13,11 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
     functions.logger.info("Hello logs!", { structuredData: true });
     response.send("Hello from Firebase!");
 });
+
+exports.api = functions.https.onRequest(app);
+
+app.get("/status", (req, res, next) => {
+    res.send({
+        status: "ok"
+    })
+})
