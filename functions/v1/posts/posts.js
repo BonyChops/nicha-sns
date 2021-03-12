2d0206dac111ad13317a295cd104b7cab234f28c
BonyChops <office.kentarou@gmail.com>
2021-03-13 00:37:46 +0900

    Edit: post api

--- functions/v1/posts/posts.js
+++ functions/v1/posts/posts.js
@@ -1,24 +1,58 @@
 const functions = require('firebase-functions');
 const express = require("express");
 const app = express();
-const { success } = require("../../returnResult");
+const moment = require('moment');
+require('moment-timezone');
+moment.tz.setDefault('Asia/Tokyo'); //Doesn't required if you think it's not necessary
+const { success, error, checkParams } = require("../../returnResult");
+const { db } = require("../../firestore");
+const rand = (min, max) => (Math.floor(Math.random() * (max - min + 1)) + min)
+const genRandomDigits = (digits) => (rand(10 ** digits, (10 ** (digits + 1)) - 1))
 
-app.get("/:id", (req, res, next) => {
-
+app.get("/:id", async(req, res, next) => {
+    const post = await db.doc(`posts/${req.params.id}`).get();
+    if(!post.exists){
+        error(res, 404, "post", "Post not found.");
+        return;
+    }
 });
 
-app.post("/", (req, res, next) => {
-    console.log(req.body)
-    success(res,req.body);
+app.post("/", async(req, res, next) => {
+    checkParams(req, res, ["content"]);
+    const time = new Date();
+    let id
+    do {
+        id = moment(time).format("YYYYMMDDHHmmSS") + genRandomDigits(4);
+    } while (await db.doc(`posts/${id}`).get().exists);
+
+    const post = {
+        id,
+        content: req.body.content,
+        timestamp: time,
+        editedTimes: 0
+    }
+
+    await db.doc(`posts/${id}`).set(post);
+    success(res, post);
     return;
 })
 
-app.put("/:id", (req, res, next) => {
-
+app.put("/:id", async(req, res, next) => {
+    const post = await db.doc(`posts/${req.params.id}`).get();
+    if(!post.exists){
+        error(res, 404, "post", "Post not found.");
+        return;
+    }
 })
 
-app.delete("/:id", (req, res, next) => {
-
+app.delete("/:id", async(req, res, next) => {
+    const post = await db.doc(`posts/${req.params.id}`).get();
+    if(!post.exists){
+        error(res, 404, "post", "Post not found.");
+        return;
+    }
+    await db.doc(`posts/${req.params.id}`).delete();
+    success(res);
 })
 
 module.exports = app;
